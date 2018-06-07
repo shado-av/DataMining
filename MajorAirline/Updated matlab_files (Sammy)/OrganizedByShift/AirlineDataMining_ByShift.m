1ee44ebe9ce8ce31da793e32a7916c9b24a7ec37
Samantha Archer <samantha.archer@duke.edu>
2018-06-07 15:43:40 -0400

    including departure distributions

--- MajorAirline/Updated matlab_files (Sammy)/OrganizedByShift/AirlineDataMining_ByShift.m
+++ MajorAirline/Updated matlab_files (Sammy)/OrganizedByShift/AirlineDataMining_ByShift.m
@@ -43,7 +43,6 @@ AM_numFlights = [];
 PM_numFlights = [];
 MID_numFlights = [];
 
-
 for desk_num = 1 : AMdesks_num
     d=strsplit(AMdesks{desk_num},'N');
     d=d{1};
@@ -154,10 +153,64 @@ xlswrite('RLSInterarrivalTimes_Overnight.xlsx', MIDtimes);
 [error_MID,dist_MID,fig_MID,v_MID]=DistributionFitter('Release','Overnight Shift', round(MIDtimes));
 fprintf(fileID1,'Overnight Shifts, %s\n',dist_MID);
 print('Release Interrarrival Times (Overnight)','-djpeg')
-%% 
-fileID2 = fopen('AMHaulData.csv', 'w');
-fileID3 = fopen('PMHaulData.csv', 'w');
-fileID4 = fopen('MIDHaulData.csv', 'w');
+
+%% To get Number of Flight Departures per shift
+
+AMdep_num = [];
+PMdep_num = [];
+MIDdep_num = [];
+
+depTable = readtable('DepartureTimeDistributions.csv');
+numDep = depTable.NumberOfFlights;
+depDesks = depTable.Desk;
+
+for desk_num = 1 : AMdesks_num
+    d=strsplit(AMdesks{desk_num},'N');
+    d=d{1};
+    for i=1:length(depDesks)
+        d2=strsplit(depDesks{i});
+        d2 = d2{2};
+        if strcmp(d2, d)
+            AMdep_num = [AMdep_num; numDep(i)];
+        end
+    end 
+end
+for desk_num = 1 : PMdesks_num
+    d=strsplit(PMdesks{desk_num},'N');
+    d=d{1};
+    for i=1:length(depDesks)
+        d2=strsplit(depDesks{i});
+        d2 = d2{2};
+        if strcmp(d2, d)
+            PMdep_num = [PMdep_num; numDep(i)];
+        end
+    end
+    
+end
+for desk_num = 1 : MIDdesks_num
+    d=strsplit(MIDdesks{desk_num},'N');
+    d=d{1};
+    for i=1:length(depDesks)
+        d2=strsplit(depDesks{i});
+        d2 = d2{2};
+        if strcmp(d2, d)
+            MIDdep_num = [MIDdep_num; numDep(i)];
+        end
+    end
+end
+
+
+[error_AM_dep,dist_AM_dep,fig_AM_dep,v_AM_dep]=DistributionFitter('Departure','Morning Shift', AMdep_num);
+print('Number of Flights Following Distribution (AM)','-djpeg')
+[error_PM_dep,dist_PM_dep,fig_PM_dep,v_PM_dep]=DistributionFitter('Long Haul','Afternoon Shift', PMdep_num);
+print('Number of Flights Following Distribution (PM)','-djpeg')
+[error_MID_dep,dist_MID_dep,fig_MID_dep,v_MID_dep]=DistributionFitter('Long Haul','Overnight Shift', MIDdep_num);
+print('Number of Flights Following Distribution (Overnight)','-djpeg')
+
+
+fileID2 = fopen('AMFlightData.csv', 'w');
+fileID3 = fopen('PMFlightData.csv', 'w');
+fileID4 = fopen('MIDFlightData.csv', 'w');
 
 [error_AM_H,dist_AM_H,fig_AM_H,v_AM_H]=DistributionFitter('Long Haul','Morning Shift', AM_longHaul);
 print('Long Haul Flight Distribution (AM)','-djpeg')
@@ -166,22 +219,24 @@ print('Long Haul Flight Distribution (PM)','-djpeg')
 [error_MID_H,dist_MID_H,fig_MID_H,v_MID_H]=DistributionFitter('Long Haul','Overnight Shift', MID_longHaul);
 print('Long Haul Flight Distribution (Overnight)','-djpeg')
 
-fprintf(fileID2,'Total Flights, Number Long Haul, Number Short Haul\n');
+fprintf(fileID2,'Total Flights, Number Long Haul, Number Short Haul, Number Departed\n');
 for i=1:AMdesks_num
-    fprintf(fileID2,'%d,%d,%d\n', AM_numFlights(i), AM_longHaul(i),AM_shortHaul(i));
+    fprintf(fileID2,'%d,%d,%d, %d\n', AM_numFlights(i), AM_longHaul(i),AM_shortHaul(i), AMdep_num(i));
 end
-fprintf(fileID2,'\n %s \n',dist_AM_H);
-
-fprintf(fileID3,'Total Flights, Number Long Haul, Number Short Haul\n');
+fprintf(fileID2,'\n ,%s,,%s \n',dist_AM_H, dist_AM_dep);
+fprintf(fileID3,'Total Flights, Number Long Haul, Number Short Haul, Number Departed\n');
 for i=1:PMdesks_num
-    fprintf(fileID3,'%d,%d,%d\n', PM_numFlights(i), PM_longHaul(i),PM_shortHaul(i));
+    fprintf(fileID3,'%d,%d,%d,%d\n', PM_numFlights(i), PM_longHaul(i),PM_shortHaul(i), PMdep_num(i));
 end
-fprintf(fileID3,'\n %s \n',dist_PM_H);
-fprintf(fileID4,'Total Flights, Number Long Haul, Number Short Haul\n');
+fprintf(fileID3,'\n ,%s,,%s \n',dist_PM_H, dist_PM_dep);
+fprintf(fileID4,'Total Flights, Number Long Haul, Number Short Haul, Number Departed\n');
 for i=1:MIDdesks_num
-    fprintf(fileID4,'%d,%d,%d\n', MID_numFlights(i), MID_longHaul(i),MID_shortHaul(i));
+    fprintf(fileID4,'%d,%d,%d,%d\n', MID_numFlights(i), MID_longHaul(i),MID_shortHaul(i), MIDdep_num(i));
 end
-fprintf(fileID4,'\n %s \n',dist_MID_H);
+fprintf(fileID4,'\n ,%s,,%s \n',dist_MID_H, dist_MID_dep);
+
+
+
 
 
 
---
+++ MajorAirline/Updated matlab_files (Sammy)/OrganizedByShift/DistributionFitter.m
@@ -0,0 +1,117 @@
+function [error,Distribution, Histogram, valid]=DistributionFitter(dataType,deskName,x)
+
+format long
+clf;
+if length(x)<4
+    valid=0;
+    error=0;
+    Distribution=0;
+    Histogram=0;
+    return; 
+end
+
+valid=1;
+
+minx=min(x);
+maxx=max(x);
+modex=mode(x);
+
+% Data
+frequencyProbs = zeros(maxx,1);
+xpos =  x;
+for i = 1:length(x)
+    if x(i)==0
+        xpos(i)=1e-10;
+    end
+end
+for i = 0:maxx
+    frequencyProbs(i+1) = sum(x==i)/length(x);
+end
+y=linspace(minx,maxx,maxx+1);
+
+% Compute PDFs (Triangular, Uniform, Exponential, Lognormal)
+if ( minx~=maxx)
+    pd1 = makedist('Triangular','a',minx,'b',modex,'c',maxx);
+    pdf1 = pdf(pd1,y);
+else
+    pdf1=zeros(maxx+1);
+end
+pdf2 = linspace(1/length(x),1/length(x),max(x)+1);
+mu1=expfit(x);
+pdf3 = exppdf(y,mu1);
+parmhat = lognfit(xpos);
+mu2=parmhat(1);
+sigma=parmhat(2);
+pdf4= lognpdf(y,mu2,sigma);
+
+
+% Plot the Data
+figure(1);
+hold on;
+H=histogram(x,maxx+2,'Normalization', 'pdf' );
+HistVals=H.Values;
+
+%Calculate Errors
+for i=1:max(x)
+    er1(i)=pdf1(i)-HistVals(i);
+    er2(i)=pdf2(i)-HistVals(i);
+    er3(i)=pdf3(i)-HistVals(i);
+    er4(i)=pdf4(i)-HistVals(i);
+    
+end
+errorTri=sum((er1).^2)/(maxx+1);
+errorUni=sum((er2).^2)/(maxx+1);
+errorExp=sum((er3).^2)/(maxx+1);
+errorLogn=sum((er4).^2)/(maxx+1);
+
+
+% Find Best Fit
+errorVec=[errorTri errorUni errorExp errorLogn];
+minErr=min(errorVec);
+
+if minErr==errorTri
+    BestFit='Triangular';
+elseif minErr==errorUni
+    BestFit='Uniform';
+elseif minErr==errorExp
+    BestFit='Exponential';
+elseif minErr==errorLogn
+    BestFit='Lognormal';
+end
+
+
+switch BestFit
+    case 'Triangular'
+        Histogram=plot(y,pdf1,'r','LineWidth',2);
+        legend('Data PDF','Triangular Distribution');
+        error=errorTri;
+        Distribution=strcat(BestFit,'(',num2str(minx),'; ', ...
+            num2str(modex),'; ',num2str(maxx),')');
+        
+    case 'Uniform'
+        Histogram=plot(y,pdf2,'y','LineWidth',2);
+        legend('Data PDF','Uniform Distribution');
+        error=errorUni;
+        Distribution=strcat(BestFit,'(',num2str(minx),'; ', ...
+            num2str(maxx),')');
+    case 'Exponential'
+        Histogram=plot(y,pdf3,'b','LineWidth',2);
+        legend('Data PDF','Exponential Distribution');
+        mu=mu1;
+        error=errorExp;
+        Distribution=strcat(BestFit,'(',num2str(mu1),')');
+    case 'Lognormal'
+        Histogram=plot(y,pdf4,'k','LineWidth',2);
+        legend('Data PDF','Lognormal Distribution');
+        error=errorLogn;
+        mu=mu2;
+        stddev=sigma;
+        Distribution=strcat(BestFit,'(',num2str(mu2),'; ' ...
+            ,num2str(sigma),')');
+        
+end
+T=strcat('Best fit of',{' '},dataType,{' '}, 'data for',{' '}, deskName);
+title(T)
+xlabel('Interarrival Time (minutes)')
+ylabel('Probability Distributions')
+hold off;
